c755d7f272db5a4458038a71a9ea6af6be9791d5
Alexandr <rozhkov.a.ju@gmail.com>
2026-06-02 17:44:55 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/com/example/aleksandr_rozkov_6020_pz1/AleksandrRozkov6020Pz1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aleksandr_rozkov_6020_pz1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AleksandrRozkov6020Pz1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
