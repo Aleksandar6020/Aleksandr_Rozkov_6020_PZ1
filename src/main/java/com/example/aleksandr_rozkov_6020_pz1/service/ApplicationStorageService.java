4b9c94a9ae7680ae77b258f024495f9bc1c1defa
Alexandr <rozhkov.a.ju@gmail.com>
2026-06-07 07:22:31 +0200

    Add application scope storage

---
+++ src/main/java/com/example/aleksandr_rozkov_6020_pz1/service/ApplicationStorageService.java
@@ -0,0 +1,43 @@
+package com.example.aleksandr_rozkov_6020_pz1.service;
+
+import com.example.aleksandr_rozkov_6020_pz1.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApplicationStorageService {
+
+    private List<Manul> manuls;
+    private List<User> users;
+    private List<Suggestion> suggestions;
+    private List<Category> categories;
+    private List<Comment> comments;
+
+    public ApplicationStorageService() {
+        manuls = new ArrayList<>();
+        users = new ArrayList<>();
+        suggestions = new ArrayList<>();
+        categories = new ArrayList<>();
+        comments = new ArrayList<>();
+    }
+
+    public List<Manul> getManuls() {
+        return manuls;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public List<Suggestion> getSuggestions() {
+        return suggestions;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+}
